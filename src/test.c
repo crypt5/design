a079f26adcb21a39a87e8359b5030050fc35ed49
Kenneth Smith <ksmi01@nmt.edu>
2015-03-09 22:14:20 -0600

    testing

--- src/test.c
+++ src/test.c
@@ -13,14 +13,17 @@ int main()
 {
   GUI* g=NULL;
   WIDGET* label=NULL;
+  WIDGET* button=NULL;
 
   g=init_gui();
   create_main_window(g,"Widget Testing");
   set_main_size(g,500,500);
 
   label=create_label("Hello World!",10,10);
+  button=create_button("Click me!",10,60);
 
   add_to_main(g,label);
+  add_to_main(g,button);
 
   show_main(g);
   while(gui_running(g)){
