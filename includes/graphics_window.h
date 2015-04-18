1a7bb945ad5bf16495367e5dd993275fa295a8a6
Kenneth Smith <ksmi01@nmt.edu>
2015-04-18 21:45:06 +0000

    Updated libs

---
+++ includes/graphics_window.h
@@ -0,0 +1,31 @@
+#ifndef GRAPHICS_WINDOW_H_
+#define GRAPHICS_WINDOW_H_
+
+#include <X11/Xlib.h>
+#include "link.h"
+#include "queue.h"
+
+typedef struct window_t WINDOW;
+struct window_t{
+  Window w;
+  LIST* widgets;
+  QUEUE* updates;
+};
+
+#include "graphics.h"
+
+// Pass negitive number to use main background
+WINDOW* create_window(GUI* g,char* title, int bgColor);
+
+void set_window_icon(GUI* g, WINDOW* w,char* filename);//TODO
+
+void set_window_size(GUI* g,WINDOW* win, int height, int width);
+
+void add_widget_to_window(WINDOW* win, WIDGET* w);//TODO
+
+void update_window_widget(WINDOW* win, WIDGET* w);//TODO
+
+void set_window_visible(GUI* g,WINDOW* win,int visible);
+
+void destroy_window(GUI* g,WINDOW* win);
+#endif
